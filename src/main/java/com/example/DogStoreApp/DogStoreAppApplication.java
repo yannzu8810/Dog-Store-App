2fbd490b211d311fd64247a7a2f81f9244359eba
森谷駿翔 <hayato.moriya@cubeedge.com>
2024-08-27 17:26:35 +0900

    プロジェクト作成しました

---
+++ src/main/java/com/example/DogStoreApp/DogStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.DogStoreApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogStoreAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DogStoreApp/DogStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DogStoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
